44e8f5be18d635258aa9ad08e79544fa0e7fab5d
Pablo Paladino <pameljuanpi@gmail.com>
2020-03-25 18:24:14 -0500

    Post con el date en su formato

--- Interface.py
+++ Interface.py
@@ -1,14 +1,15 @@
 #feed, post y seguidos
 import datetime
 import csv
-user_input = 0
 
-def post(user):
+def post(user): #permite hacer posts
     new_post = input("Haz tu post (maximo 140 caracteres): \n")
 
     post_date = datetime.datetime.now()
-    real_postdate = str(post_date.year + "/" + post_date.month + "/" + post_date.day)
+    real_postdate = str(post_date.year) + "/" + str(post_date.month) + "/" + str(post_date.day)
 
     if len(new_post) <= 140:
         with open("posts.csv", "a") as file:
-            file.write(user + "," + str(post_date) + "," + new_post)
+            file.write(user + "," + real_postdate + "," + new_post)
+    else:
+        print("Tu post debe tener 140 caracteres o menos")
