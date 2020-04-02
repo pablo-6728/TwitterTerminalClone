79fdc2b1969ea27d9dda7633716200997dfb5f94
Pablo Paladino <pameljuanpi@gmail.com>
2020-04-02 10:17:13 -0500

    uso de dos arrays para guardar usuarios seguidos y base de usuarios seguidos

--- Interface.py
+++ Interface.py
@@ -19,87 +19,77 @@ def post(user):  # permite hacer posts
         return print("Tu post debe tener menos de 140")
 
 
-    elif len(new_post)==0:  # post vacio
+    elif len(new_post) == 0:  # post vacio
         return print("Tu post no puede estar vacio")
 
 
 def feed(user):
     post_feed = []
 
-
     with open("seguidores.csv", "r") as file:
 
         reader = csv.reader(file)
 
-
-        for row in reader:          #los seguidores se guardan en posr feed desde el indice 1
+        for row in reader:  # los seguidores se guardan en posr feed desde el indice 1
             if row[0] == user:
                 post_feed = row
 
-    #tratemos de hacer el sort antes de imprimir
+    # tratemos de hacer el sort antes de imprimir
     sort_fechas = open("posts.csv", "r")
 
-    csvr = csv.reader(sort_fechas, delimiter = ",")
+    csvr = csv.reader(sort_fechas, delimiter=",")
 
     sort = sorted(csvr, key=operator.itemgetter(1))
-
 
     for row in sort:
         if row[0] in post_feed:
             print(row)
 
-def follow(user): #funcion para hacer follow
+
+def follow(user):  # funcion para hacer follow
 
     follow_user = input("Escribe el nombre de usuario de la persona que deseas seguir: ")
     existing_user = False
 
-    with open("usuarios.csv", "rt") as file:         #revisar si el usuario existe
+    with open("usuarios.csv", "rt") as file:  # revisar si el usuario existe
         reader = csv.reader(file)
 
         for column in reader:
             if follow_user in column:
                 existing_user = True
 
-            else:
-                existing_user = False
-
+    print(existing_user)
 
     if existing_user == True:
 
-        if follow_user == user:     #chequeo de uno mismo
+        if follow_user == user:  # chequeo de uno mismo
             print("No te puedes seguir a ti mismo #sad")
 
         with open("seguidores.csv", "rt") as file:
             reader = csv.reader(file)
             followed_users = []
+            seguidores = []  # nuevo csv
+
 
             for row in reader:
+                seguidores = row        #asignar en seguidores toda la lista de la data base
+                print(seguidores)
+
                 if user in row[0]:
                     followed_users = row
+                    print("///")
                     print(followed_users)
 
 
-                    if follow_user in followed_users:    #ya se sigue a este usuario
-                        print("Ya sigues a este usuario")
 
 
-                    else:       #este usuario no se sigue
-                        with open ("seguidores.csv", "a") as file:
-                            if user in row[0]:
-                                file.write(follow_user + ",")
+        if follow_user in followed_users:  # ya se sigue a este usuario
+            print("Ya sigues a este usuario")
+
+
+        else:  # este usuario no se sigue
+            pass
+
 
     elif existing_user == False:
         print("El usuario no existe")
-
-
-
-
-
-
-
-
-
-
-
-
-
